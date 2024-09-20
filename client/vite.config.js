e816605b924c5b5ced896e9c31bfa4e126e2f8b6
aniransingh <aniransingh19@gmail.com>
2024-09-21 01:05:11 +0530

    chore: dockerize app, add service orchestration using docker compose, add reverse proxy using caddy server

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,10 +4,12 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
     server: {
+        host: true,
         proxy: {
             '/api': {
-                target: 'http://localhost:4000',
+                target: 'http://api:4000',
                 changeOrigin: true,
+                // rewrite: (path) => path.replace(/^\/api/, '')
             },
         },
     },
